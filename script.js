bd82a115dd1e1bbf1d5679919964a4ccc05f57ab
José Delpino <delpinoivivas@gmail.com>
2023-07-10 11:57:57 -0500

    Added more JSDocs style documentation to the main js file

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
  *
  */
 
+//** Global variables */
 /** @type {HTMLInputElement} */
 const input = document.querySelector("#fruit");
 
@@ -95,14 +96,40 @@ const fruit = [
   "Yuzu",
 ];
 
+//** Main program */
+initializeApp();
+
+//** Subroutines */
+
 /**
- * Brief description of the function.
+ * This fuctions initializes the app and loads
+ * the info saved in local storage in case it is available.
  *
  * @param {paramType} paramName - Description of the parameter.
  * @returns {returnType} Description of the return value.
  */
+function initializeApp() {
+  loadStettings();
+  loadStorage();
+  registerEventListeners();
+}
 
-function search(str) {
+/** Main program subroutines */
+
+/** This function registers the event listeners for the app.
+ * @returns {undefined}
+ */
+function registerEventListeners() {
+  input.addEventListener("keyup", searchHandler);
+  suggestions.addEventListener("click", useSuggestion);
+}
+
+/** This function searches for the string in the fruit array.
+ * @param {string} string - The string to search for.
+ * @returns {Array.<string>} - An array with the results.
+ * @todo Implement this function.
+ */
+function search(string) {
   let results = [];
 
   // TODO
@@ -110,17 +137,114 @@ function search(str) {
   return results;
 }
 
-function searchHandler(e) {
+/** This function handles the search event.
+ * @param {Event} event - The event object.
+ * @returns {undefined}
+ * @todo Implement this function.
+ */
+function searchHandler(event) {
   // TODO
 }
 
+/** This function shows the suggestions in the suggestions list.
+ * @param {Array.<string>} results - The results to show.
+ * @param {string} inputVal - The value of the input field.
+ * @returns {undefined}
+ * @todo Implement this function.
+ */
 function showSuggestions(results, inputVal) {
   // TODO
 }
 
-function useSuggestion(e) {
+/** This function uses the suggestion selected by the user.
+ * @param {Event} event - The event object.
+ * @returns {undefined}
+ * @todo Implement this function.
+ */
+function useSuggestion(event) {
   // TODO
 }
 
-input.addEventListener("keyup", searchHandler);
-suggestions.addEventListener("click", useSuggestion);
+/** This function clears the suggestions list.
+ * @returns {undefined}
+ * @todo Implement this function.
+ */
+function clearSuggestions() {
+  // TODO
+}
+
+//** Settings subroutines */
+
+/** This function loads the settings from local storage.
+ * @returns {undefined}
+ * @todo Implement this function.
+ */
+function loadStettings() {
+  // TODO
+}
+
+/** This function creates the settings in local storage.
+ * @returns {undefined}
+ * @todo Implement this function.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+ */
+
+//** Local storage subroutines */
+
+/** This function loads the data from local storage.
+ * @returns {undefined}
+ * @todo Implement this function.
+ */
+function loadStorage() {
+  // TODO
+}
+
+/** This function saves the data to local storage.
+ * @returns {undefined}
+ * @todo Implement this function.
+ */
+function saveStorage() {
+  // TODO
+}
+
+/** This function clears the data from local storage.
+ * @returns {undefined}
+ * @todo Implement this function.
+ */
+function clearStorage() {
+  // TODO
+}
+
+/** This function checks if local storage is available.
+ * @returns {boolean} - True if local storage is available, false otherwise.
+ * @todo Implement this function.
+ */
+function isStorageAvailable() {
+  // TODO
+}
+
+/** This function checks if local storage is empty.
+ * @returns {boolean} - True if local storage is empty, false otherwise.
+ * @todo Implement this function.
+ */
+
+function isStorageEmpty() {
+  // TODO
+}
+
+/** This function checks if local storage is full.
+ * @returns {boolean} - True if local storage is full, false otherwise.
+ * @todo Implement this function.
+ */
+function isStorageFull() {
+  // TODO
+}
+
+/** This function checks if local storage is supported.
+ * @returns {boolean} - True if local storage is supported, false otherwise.
+ * @todo Implement this function.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+ */
+function isStorageSupported() {
+  // TODO
+}
